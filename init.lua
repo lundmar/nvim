9e121436dd634ddba6e75823f7c13e9940e2744e
Martin Lund <martin.lund@keep-it-simple.com>
2022-08-21 14:50:38 +0200

    Update

--- init.lua
+++ init.lua
@@ -33,6 +33,15 @@ vim.o.textwidth = 300
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.softtabstop = -1 -- If negative, shiftwidth value is used
+vim.o.showtabline = 2
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 8
+vim.o.signcolumn = "yes"
+vim.o.wrap = false
+vim.o.cursorline = true
+vim.o.number = true
+vim.o.numberwidth = 2
+vim.o.ignorecase = true
 
 -- Makes neovim and host OS clipboard play nicely with each other
 vim.o.clipboard = 'unnamedplus'
