24699c4a907b8e91b6b2d9c0d4f737456ba094f8
Martin Lund <martin.lund@keep-it-simple.com>
2022-08-27 19:37:55 +0200

    Update

--- init.lua
+++ init.lua
@@ -233,7 +233,8 @@ return require('packer').startup(function(use)
     use
     {
         'sindrets/diffview.nvim',
-        requires = 'nvim-lua/plenary.nvim'
+        requires = 'nvim-lua/plenary.nvim',
+        config = function() require('diffview').setup({use_icons = false}) end
     }
 
     -- Colorschemes
