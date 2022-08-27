44327b9e31c956e8b628e342b2060c21b44135d6
Martin Lund <martin.lund@keep-it-simple.com>
2022-08-27 07:22:50 +0200

    Fix loading of plugins. Cleanup.

--- init.lua
+++ init.lua
@@ -67,15 +67,6 @@ vim.o.smartcase = true
 
 -- PLUGIN CONFIGURATION --
 
--- Enable lualine
-require('lualine').setup()
-
--- Enable LSP servers
-require'lspconfig'.clangd.setup{}
-
--- Enable gitsigns
-require('gitsigns').setup()
-
 -- Key to enable Tagbar
 vim.api.nvim_set_keymap('n', '<F8>', ':TagbarToggle<CR>', { noremap = false, silent = true })
 
@@ -109,7 +100,7 @@ require'nvim-treesitter.configs'.setup {
 -- Setup nvim-cmp
 vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 
-local cmp = require'cmp'
+local cmp = require('cmp')
 
 cmp.setup({
     snippet = {
@@ -171,7 +162,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Enable colorscheme preview in telescope
-require "telescope".setup {
+require('telescope').setup {
   pickers = {
     colorscheme = {
       enable_preview = true
@@ -193,7 +184,11 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     -- Language server stuff
-    use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+    use
+    {
+        'neovim/nvim-lspconfig',
+        config = function() require('lspconfig').clangd.setup{} end
+    }
 
     -- Completion stuff
     use 'hrsh7th/cmp-nvim-lsp'
@@ -205,7 +200,11 @@ return require('packer').startup(function(use)
     use 'hrsh7th/vim-vsnip'
 
     -- Status line stuff
-    use 'nvim-lualine/lualine.nvim'
+    use
+    {
+        'nvim-lualine/lualine.nvim',
+        config = function() require('lualine').setup() end
+    }
 
     -- Syntax highlighting stuff
     use
@@ -224,7 +223,11 @@ return require('packer').startup(function(use)
     use 'ludovicchabant/vim-gutentags'
 
     -- git stuff
-    use 'lewis6991/gitsigns.nvim'
+    use
+    {
+        'lewis6991/gitsigns.nvim',
+        config = function() require('gitsigns').setup() end
+    }
 
     -- Colorschemes
     use 'RRethy/nvim-base16'
@@ -235,4 +238,12 @@ return require('packer').startup(function(use)
         'nvim-telescope/telescope.nvim', branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
+
+    -- Manage comments Better
+    use
+    {
+        'terrortylor/nvim-comment',
+        config = function() require('nvim_comment').setup() end
+    }
+
 end)
