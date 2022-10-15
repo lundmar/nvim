c7c803f52340a4296fb4d1ef3d5ebd6a09e6e4ac
Martin Lund <martin.lund@keep-it-simple.com>
2022-10-15 20:44:50 +0200

    Update

--- init.lua
+++ init.lua
@@ -262,7 +262,7 @@ require('telescope').setup {
 }
 
 -- Setup lsp-config
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['clangd'].setup {
