216b80221e0c599d1cf388f77189bb9f9258402c
Martin Lund <martin.lund@keep-it-simple.com>
2022-11-10 21:28:07 +0100

    Update

--- init.lua
+++ init.lua
@@ -73,20 +73,6 @@ vim.opt.smartcase = true
 
 -- PLUGIN CONFIGURATION --
 
--- Automatically install package manager
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
-end
-
-local packer_bootstrap = ensure_packer()
-
 -- Enable packer package manager
 require('packer').startup(function(use)
     -- Packer can manage itself
@@ -177,11 +163,6 @@ require('packer').startup(function(use)
         config = function () require'alpha'.setup(require'alpha.themes.startify'.config) end
     }
 
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if packer_bootstrap then
-        require('packer').sync()
-    end
 end)
 
 -- Key to enable Tagbar
