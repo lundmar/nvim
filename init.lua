40d73a058ecbe0322c8067492af4a8fc826504b3
Martin Lund <martin.lund@keep-it-simple.com>
2022-11-11 22:42:03 +0100

    Update

--- init.lua
+++ init.lua
@@ -64,7 +64,10 @@ require('packer').startup(function(use)
     use
     {
         'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
     }
 
     -- Function list stuff
